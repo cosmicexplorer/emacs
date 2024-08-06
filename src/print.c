eb43c0df9c772b120780864154b2cd83bcd500f9
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2024-08-06 07:42:10 -0400

    convert Lisp_Regexp->buffer and Lisp_Match->regs to inline over pointers

--- src/print.c
+++ src/print.c
@@ -2088,10 +2088,10 @@ print_vectorlike_unreadable (Lisp_Object obj, Lisp_Object printcharfun,
 	struct Lisp_Regexp *r = XREGEXP (obj);
 	print_c_string ("#<regexp pattern=", printcharfun);
 	print_object (r->pattern, printcharfun, escapeflag);
-	int i = sprintf (buf, " nsub=%ld", r->buffer->re_nsub);
+	int i = sprintf (buf, " nsub=%ld", r->buffer.re_nsub);
 	strout (buf, i, i, printcharfun);
 	print_c_string (" translate=", printcharfun);
-	print_object (r->buffer->translate, printcharfun, escapeflag);
+	print_object (r->buffer.translate, printcharfun, escapeflag);
 	print_c_string (" whitespace=", printcharfun);
 	print_object (r->whitespace_regexp, printcharfun, escapeflag);
 	print_c_string (" syntax_table=", printcharfun);
@@ -2111,7 +2111,7 @@ print_vectorlike_unreadable (Lisp_Object obj, Lisp_Object printcharfun,
     case PVEC_MATCH:
       {
 	struct Lisp_Match *m = XMATCH (obj);
-	ptrdiff_t num_regs = m->regs->num_regs;
+	ptrdiff_t num_regs = m->regs.num_regs;
 	ptrdiff_t initialized_regs = m->initialized_regs;
 	print_c_string ("#<match", printcharfun);
 	int i = sprintf (buf, " num_regs=%ld(%ld allocated)",
@@ -2123,8 +2123,8 @@ print_vectorlike_unreadable (Lisp_Object obj, Lisp_Object printcharfun,
 	for (ptrdiff_t reg_index = 0; reg_index < num_regs; ++reg_index)
 	  {
 	    int i = sprintf (buf, "(%ld,%ld),",
-			     m->regs->start[reg_index],
-			     m->regs->end[reg_index]);
+			     m->regs.start[reg_index],
+			     m->regs.end[reg_index]);
 	    strout (buf, i, i, printcharfun);
 	  }
 	print_c_string ("]>", printcharfun);
